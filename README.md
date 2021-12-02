7bfc8a7360b1d74b2b97c0a486e9bac59289cce5
Josh Cai <yuanshi419@gmail.com>
2021-12-01 21:01:56 -0800

    Add 2021 day01 solution

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Advent of Code (adventofcode.com) solutions
 
 | Year | Language |
 |------|----------|
+| 2021 | Kotlin   |
 | 2020 | Go       |
 | 2019 | Clojure  |
---
+++ src/2021/day01.kt
@@ -0,0 +1,44 @@
+import java.io.File
+
+fun readFile(file: String): List<Int> {
+    return File(file)
+            .readLines()
+            .map(String::toInt)
+}
+
+fun part1(file: String): Int {
+    val numbers = readFile(file)
+    var curr = numbers[0]
+    var counter = 0
+    for (num in numbers) {
+        if (num > curr) {
+            counter += 1
+        }
+        curr = num
+    }
+    return counter
+}
+
+fun part2(file: String): Int {
+    val numbers = readFile(file)
+    var curr = numbers[0] + numbers[1] + numbers[2]
+    var counter = 0
+    for (i in numbers.indices) {
+        if (i < 3) {
+            continue
+        }
+        val next = curr - numbers[i-3] + numbers[i]
+        if (next > curr) {
+            counter += 1
+        }
+        curr = next
+    }
+    return counter
+}
+
+fun main() {
+    check(part1("resources/2021/day01/test") == 7)
+    println(part1("resources/2021/day01/input"))
+    check(part2("resources/2021/day01/test") == 5)
+    println(part2("resources/2021/day01/input"))
+}
